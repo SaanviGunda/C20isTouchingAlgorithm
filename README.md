48368875358e9cdad0178bc7026dca2cc4e8f22d
SaanviGunda <76458299+SaanviGunda@users.noreply.github.com>
2021-01-17 18:43:53 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C20isTouchingAlgorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,23 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  movingRect = createSprite(400, 400, 25, 25);
+}
+
+function draw() {
+  background(255,255,255); 
+  movingRect.y=mouseY;
+  movingRect.x=mouseX;
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 && fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2
+    && fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 && movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2){
+    movingRect.shapeColor = 'yellow';
+    fixedRect.shapeColor = 'yellow';
+  } 
+  else{
+    movingRect.shapeColor = 'red';
+    fixedRect.shapeColor = 'red';   
+  }
+  drawSprites();
+}
